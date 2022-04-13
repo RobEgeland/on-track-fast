77935a8132fda91764a8e2c7a0c49d136b01937b
Robert Egeland <robegeland@yahoo.com>
2022-04-12 21:31:09 -0500

    fix react router dom, filled out home component and add to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import { BroswerRouter as Router, Switch, Router } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={ Home }/>
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={ <Home /> }/>
+      </Routes>
     </Router>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,12 @@
-ReadableStreamDefaultController
+import React from "react";
+
+function Home() {
+    
+    return (
+        <div>
+            <h1>welcome to on track fast</h1>
+        </div>
+    )
+}
+
+export default Home;
