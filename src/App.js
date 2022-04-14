fca3013baaebe216b7798319d3771d5ec493fd5e
Robert Egeland <robegeland@yahoo.com>
2022-04-13 19:02:54 -0500

    add navbar components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
         <Route exact path="/" element={ <Home /> }/>
       </Routes>
---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function NavBar() {
+    return(
+        <div>
+            <ul>
+                <li>home</li>
+                <li>login</li>
+                <li>sign up</li>
+                <li>buy car</li>
+                <li>sell car</li>
+            </ul>
+        </div>
+    )
+}
+
+
+export default NavBar;
