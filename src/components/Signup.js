17b64a39de88d0da6d6413fc9dd1dda1adad0214
Robert Egeland <robegeland@yahoo.com>
2022-04-21 18:05:13 -0500

    add controled components to signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,8 +2,22 @@ import React from "react";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
 
 function SignUp() {
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const navigate = useNavigate();
+
+    function handleSubmit() {
+        alert("thank you for signing up!")
+        setUsername("")
+        setEmail("")
+        navigate("/")
+    }
+
     return (
         <>
             <h1 className="signup">Signup!</h1>
@@ -17,9 +31,9 @@ function SignUp() {
                 autoComplete="off"
                 
                 >
-                    <TextField label="Username" variant="outlined" />
-                    <TextField label="Email" variant="outlined" />
-                    <Button variant="contained" >Signup</Button>
+                    <TextField label="Username" value={username} onChange={(e) => setUsername(e.target.value)} variant="outlined" />
+                    <TextField label="Email" value={email} onChange={(e) => setEmail(e.target.value)} variant="outlined" />
+                    <Button variant="contained" onClick={handleSubmit} >Signup</Button>
 
                 </Box>
         </>
